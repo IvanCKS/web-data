d0a0234bbeb75e834e7cb3560b2988b7e5394149
IvanV <kiber.king12@yandex.ru>
2026-03-09 15:34:28 +0300

    Add some logic

--- src/parcer.py
+++ src/parcer.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://example.com"
+url = "https://vk.com"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
